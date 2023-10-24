63e2ff4d0e5fef59d39668b6ae5922ef4e68460c
Flow <highflowey@gmail.com>
2023-10-25 03:16:59 +0330

    documents

---
+++ lib/docs.lua
@@ -0,0 +1,93 @@
+--[=[
+@class Identifier 
+
+Only usable in properties list
+]=]
+
+--[=[
+@class Module
+]=]
+
+--[=[
+@prop Connect Identifier
+@within Module
+]=]
+
+--[=[
+@prop Once Identifier
+@within Module
+]=]
+
+--[=[
+@prop Merge Identifier
+@within Module
+]=]
+
+--[=[
+@prop Children Identifier
+@within Module
+]=]
+
+--[=[
+@prop Init Identifier
+@within Module
+]=]
+
+--[=[
+@function key
+@within Module 
+
+@param v any -- default value
+@return Key
+]=]
+
+--[=[
+@class Key 
+
+When changed, it will automatically update the properties it's attached to
+]=]
+
+--[=[
+@prop value any
+@within Key
+]=]
+
+--[=[
+@prop updated Signal
+@within Key
+]=]
+
+--[=[
+@function new
+@within Module 
+
+@param className string
+@return Class
+]=]
+
+--[=[
+@class Class
+]=]
+
+--[=[
+@prop obj Instance
+@within Class
+]=]
+
+--[=[
+@method Render
+@within Class 
+
+@param properties {} -- list of properties and identifiers
+@param archivable boolean -- list will not apply to clones if set to false (default is true)
+@return Class
+]=]
+
+--[=[
+@method Clone
+@within Class 
+
+@return Class
+]=]
+
+return true
