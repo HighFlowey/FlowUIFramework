6353a4bc44eefc2a58e0c4da4d0153f35d523265
Flow <highflowey@gmail.com>
2023-10-25 17:10:00 +0330

    1.1.6

--- src/Class.lua
+++ src/Class.lua
@@ -44,7 +44,7 @@ function class:Render(list: {}, archivable: boolean)
 		elseif info[1] == Identifiers.Init then
 			task.defer(v, self)
 		elseif info[1] == Identifiers.Merge then
-			self:Render({ v })
+			self:Render(v)
 		elseif info[1] == Identifiers.Children then
 			for i, v in v do
 				v:Render({ Parent = self.obj })
